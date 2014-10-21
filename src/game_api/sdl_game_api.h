66bb940d5d272a1934965053804215410fce6455
Paul <crazy046@hotmail.co.uk>
2014-10-21 21:08:17 +0100

    Header to include all

---
+++ src/game_api/sdl_game_api.h
@@ -0,0 +1,13 @@
+
+#ifndef SDL_GAME_API_H_
+#define SDL_GAME_API_H_
+
+#include "misc/hashmap.h"
+#include "files/ini_reader.h"
+
+#include "graphics/sdl_window.h"
+#include "graphics/sdl_image_load.h"
+#include "graphics/sdl_image_draw.h"
+
+
+#endif /* SDL_GAME_API_H_ */
