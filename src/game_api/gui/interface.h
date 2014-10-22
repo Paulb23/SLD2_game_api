68d621262f2f72870c679b9ae75105a00dbf2ed9
Paul <crazy046@hotmail.co.uk>
2014-10-22 21:38:25 +0100

    added function prototypes + build

--- src/game_api/gui/interface.h
+++ src/game_api/gui/interface.h
@@ -16,6 +16,11 @@ Interface *interface_create();
 void interface_add_text_button(Interface *interface, Text_Button *text_button);
 void interface_add_image_button(Interface *interface, Image_Button *image_Button);
 void interface_add_check_box(Interface *interface, Check_box *check_box);
+
+void interface_remove_text_button(Interface *interface, Text_Button *text_button);
+void interface_remove_image_button(Interface *interface, Image_Button *image_button);
+void interface_remove_check_box(Interface *interface, Check_box *check_box);
+
 void interface_draw(Interface *interface, Window *window);
 void interface_update(Interface *interface, SDL_Event event);
 
