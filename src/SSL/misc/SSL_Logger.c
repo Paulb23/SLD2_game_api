673eed08c8f3d97cfec82a0e714128b9cf3dd00a
Paul <crazy046@hotmail.co.uk>
2014-12-08 19:31:21 +0000

    updated log function

--- src/SSL/misc/SSL_Logger.c
+++ src/SSL/misc/SSL_Logger.c
@@ -19,6 +19,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 
 
 /*---------------------------------------------------------------------------
@@ -60,8 +62,18 @@ void SSL_Log_Write(const char *text, ...) {
 	/* if we are logging */
 	if (LOGGIN) {
 
+		char format[250];
+		int count = 0;
+		int i, j;
+		char c;
+		double d;
+		unsigned u;
+		char *s;
+		void *v;
+
+
 		/* open the file */
-		FILE *file = fopen(LOG_FILE_PATH, "ab");
+		FILE *file = fopen(LOG_FILE_PATH, "a+");
 
 		/* if we could not open the file return */
 		if (file == NULL) {
@@ -80,7 +92,68 @@ void SSL_Log_Write(const char *text, ...) {
 
 		/* print out the message */
 		va_start(arg, text);
-		vfprintf(file, text, arg);
+			while (*text) {
+				for (j = 0; text[j] && text[j] != '%'; j++)
+				  format[j] = text[j];
+				if (j) {
+				  format[j] = '\0';
+				  count += fprintf(file, format);
+				  text += j;
+				} else {
+				  for (j = 0; !isalpha(text[j]); j++) {
+					format[j] = text[j];
+					if (j && text[j] == '%')
+					  break;
+				  }
+				  format[j] = text[j];
+				  format[j + 1] = '\0';
+				  text += j + 1;
+
+				  switch (format[j]) {
+				  case 'd':
+				  case 'i':
+					i = va_arg(arg, int);
+					count += fprintf(file, format, i);
+					break;
+				  case 'o':
+				  case 'x':
+				  case 'X':
+				  case 'u':
+					u = va_arg(arg, unsigned);
+					count += fprintf(file, format, u);
+					break;
+				  case 'c':
+					c = (char) va_arg(arg, int);
+					count += fprintf(file, format, c);
+					break;
+				  case 's':
+					s = va_arg(arg, char *);
+					count += fprintf(file, format, s);
+					break;
+				  case 'f':
+				  case 'e':
+				  case 'E':
+				  case 'g':
+				  case 'G':
+					d = va_arg(arg, double);
+					count += fprintf(file, format, d);
+					break;
+				  case 'p':
+					v = va_arg(arg, void *);
+					count += fprintf(file, format, v);
+					break;
+				  case 'n':
+					count += fprintf(file, "%d", count);
+					break;
+				  case '%':
+					count += fprintf(file, "%%");
+					break;
+				  default:
+					fprintf(stderr, "Invalid format specifier in log().\n");
+					break;
+				  }
+				}
+			  }
 		va_end(arg);
 
 		/* print a new line */
