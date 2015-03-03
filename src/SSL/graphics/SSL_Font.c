c45e58b037771d0f0e960dbc631c332eb681fe6f
Paul <crazy046@hotmail.co.uk>
2015-03-03 00:32:53 +0000

    basic bitmap font drawing

--- src/SSL/graphics/SSL_Font.c
+++ src/SSL/graphics/SSL_Font.c
@@ -199,7 +199,7 @@ void SSL_Font_Draw(int x, int y, int angle, SDL_RendererFlip flip, char *text, S
 
 				if (frame != 0) {
 					SSL_Image_Draw(font->img_font, x, y, angle, frame, flip, window);
-					x += x_inc;
+					x += x_inc + (x_inc / 4);
 				}
 			}
 		}
